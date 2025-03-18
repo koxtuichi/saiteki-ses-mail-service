7224c335cca0991a538dfbc36195eb1846af9b08
杉本光一 <sugimotokouichi@koichimac.local>
2025-03-18 23:21:19 +0900

    コードをリファクタリングし、モジュール化して可読性を向上

---
+++ config.mjs
@@ -0,0 +1,8 @@
+// 定数と設定の定義
+export const YOIN = "要員";
+export const ANKEN = "案件";
+export const SPREADSHEET_ID = "1qwV_yNutfk7QpKQ2qQPRQx7wym0u4px5h5dIqDacy4s";
+export const API_YOIN_URL =
+  "https://app.saitekiinc.com/version-221ys/api/1.1/obj/要員情報";
+export const API_ANKEN_URL =
+  "https://app.saitekiinc.com/version-221ys/api/1.1/obj/案件情報";
